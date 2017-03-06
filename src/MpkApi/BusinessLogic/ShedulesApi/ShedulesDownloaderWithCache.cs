fa58aab318cab9bc2963e0c41395638f38dd6257
marcin <marcin93w@gmail.com>
2017-03-06 21:02:38 +0100

    Added cache invalidation

--- src/MpkApi/BusinessLogic/ShedulesApi/ShedulesDownloaderWithCache.cs
+++ src/MpkApi/BusinessLogic/ShedulesApi/ShedulesDownloaderWithCache.cs
@@ -9,17 +9,17 @@ namespace MpkApi.BusinessLogic.ShedulesApi
     public class ShedulesDownloaderWithCache : IShedulesDownloader
     {
         private readonly IShedulesDownloader _shedulesDownloader;
-        private readonly Dictionary<Tuple<int, string>, Shedule> _responsesCache;
+        private readonly Dictionary<Tuple<int, string, DateTime>, Shedule> _responsesCache;
 
         public ShedulesDownloaderWithCache(IShedulesDownloader shedulesDownloader)
         {
             _shedulesDownloader = shedulesDownloader;
-            _responsesCache = new Dictionary<Tuple<int, string>, Shedule>();
+            _responsesCache = new Dictionary<Tuple<int, string, DateTime>, Shedule>();
         }
 
         public async Task<Shedule> DownloadShedule(int pointId, string lineName)
         {
-            var input = new Tuple<int, string>(pointId, lineName);
+            var input = new Tuple<int, string, DateTime>(pointId, lineName, DateTime.Today);
             if (!_responsesCache.ContainsKey(input))
             {
                 var response = await _shedulesDownloader.DownloadShedule(pointId, lineName);

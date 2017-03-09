1515783d4c0ebb3fa76733265e6854a551c969e6
marcin <marcin93w@gmail.com>
2017-03-09 20:45:10 +0100

    Fixed docker build

--- build&run.bat
+++ build&run.bat
@@ -1,10 +1,12 @@
 cd database
 docker build -t mpkkrakowapidatabase .
-docker run --name mkapostgis -e POSTGRES_PASSWORD=postgres -d mpkkrakowapidatabase
+docker run --name mkapostgis -e POSTGRES_PASSWORD=supersecretpassword -d mpkkrakowapidatabase
 
 cd ..\src\MpkApi
 
 dotnet restore
 dotnet publish -c Release -o out
 docker build -t mpkkrakowapi .
-docker run --link mkapostgis:postgres -d -p 80:80 mpkkrakowapi
+docker run --link mkapostgis:postgres -e DBInfo__ConnectionString="User ID=postgres;Password=supersecretpassword;Host=postgres;Port=5432;Database=postgres;Pooling=true;" -d -p 80:80 mpkkrakowapi
+
+cd ../..
